1fc9c9b66f3c469645a020aa38548998c181ebde
Morgane Thiebaut <morganethiebaut77@gmail.com>
2025-09-14 22:04:20 -0400

    scss+message erreur

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -5,7 +5,7 @@ let btnSuivant;
 let btnPrecedent;
 let btnSoumettre;
 let section;
-let messagesJSON = {};
+let messagesJSON;
 function afficherEtape(etape) {
     const etapes = document.querySelectorAll('section');
     cacherSection();
@@ -34,8 +34,11 @@ function cacherSection() {
     });
 }
 function naviguerSuivant(event) {
-    etape++;
-    afficherEtape(etape);
+    event.preventDefault();
+    if (validerEtape(etape)) {
+        etape++;
+        afficherEtape(etape);
+    }
 }
 function naviguerPrecedent(event) {
     console.log('naviguerPrecedent');
@@ -51,43 +54,122 @@ async function obtenirMessage() {
 ;
 function validerChamps(champ) {
     let valide = false;
+    const id = champ.id; // email
+    const idMessageErreur = "erreur-" + id; // erreur-email
+    const labelErreur = "label-" + id;
+    const erreurElement = document.getElementById(idMessageErreur);
+    const erreurLabel = document.getElementById(labelErreur);
     // Vérifie chaque type d'erreur de validation
-    if (champ.validity.valueMissing && messagesJSON?.vide) {
-        // Champ obligatoire vide (attribut required)
+    if (champ.validity.valueMissing && messagesJSON[id]?.vide) {
+        console.log('erreur', id);
         valide = false;
+        erreurElement.innerText = messagesJSON[id]?.vide;
+        erreurLabel.classList.add("erreur");
     }
-    else if (champ.validity.typeMismatch && messagesJSON?.typeMismatch) {
+    else if (champ.validity.typeMismatch && messagesJSON[id].type) {
         // Type de données incorrect (email, url, tel, etc.)
         valide = false;
+        erreurElement.innerText = messagesJSON[id]?.type;
+        erreurLabel.classList.add("erreur");
     }
-    else if (champ.validity.patternMismatch && messagesJSON?.patternMismatch) {
+    else if (champ.validity.patternMismatch && messagesJSON[id]?.pattern) {
         // Ne correspond pas au pattern regex défini
         valide = false;
+        erreurElement.innerText = messagesJSON[id]?.pattern;
+        erreurLabel.classList.add("erreur");
     }
-    else if (champ.validity.valid === false) {
-        // Catch-all pour toute autre erreur de validation non spécifique
+    else {
+        // La validation n'a pas d'erreur, donc on assigne la variable vraie
+        valide = true;
+        erreurElement.innerText = "";
+    }
+    return valide;
+}
+function validerEmail(champ) {
+    let valide = false;
+    const id = "email";
+    const leEmail = champ.value;
+    const idMessageErreur = "erreur-" + id; // erreur-email
+    const erreurElement = document.getElementById(idMessageErreur);
+    const tldSuspicieux = [
+        '.ru',
+        '.cn',
+        'click',
+        '.party',
+    ];
+    const erreursCommune = {
+        'hotnail': 'hotmail',
+    };
+    const tldValide = tldSuspicieux.some((tld) => {
+        const contientSuspect = leEmail.toLowerCase().endsWith(tld);
+        return contientSuspect;
+    });
+    // Vérifie chaque type d'erreur de validation
+    if (champ.validity.valueMissing && messagesJSON[id]?.vide) {
+        // Champ obligatoire vide (attribut required)
         valide = false;
+        erreurElement.innerText = messagesJSON[id]?.vide;
     }
+    else if (champ.validity.typeMismatch && messagesJSON[id]?.type) {
+        // Type de données incorrect (email, url, tel, etc.)
+        valide = false;
+        erreurElement.innerText = messagesJSON[id]?.type;
+    }
+    else if (champ.validity.patternMismatch && messagesJSON[id]?.pattern) {
+        // Ne correspond pas au pattern regex défini
+        valide = false;
+        erreurElement.innerText = messagesJSON[id]?.pattern;
+    }
+    else if (tldValide && messagesJSON[id].tldSuspicieux) {
+        valide = false;
+        erreurElement.innerText = messagesJSON[id].tldSuspicieux;
+    }
+    else {
+        const erreursCles = Object.keys(erreursCommune);
+        const erreurCle = erreursCles.find((domaine) => {
+            return leEmail.toLowerCase().includes(domaine);
+        });
+        valide = true;
+    }
+    return valide;
 }
 function validerEtape(etape) {
     let etapeValide = false;
     switch (etape) {
         case 0:
+            const montantElement = document.getElementById('montant');
+            etapeValide =
+                validerChamps(montantElement);
+            break;
+        case 1:
             const prenomElement = document.getElementById('prenom');
             const nomElement = document.getElementById('nom');
             const telephoneElement = document.getElementById('telephone');
             const emailElement = document.getElementById('email');
-            etapeValide;
-            etapeValide = validerChamps(prenomElement) && validerChamps(nomElement) && validerChamps(nomElement);
-            if (etapeValide) {
-                etapeValide = validerChamps(nomElement);
-            }
-            if (etapeValide) {
-                etapeValide = validerChamps(telephoneElement);
-            }
-            if (etapeValide) {
-                etapeValide = validerChamps(emailElement);
-            }
+            const villeElement = document.getElementById('ville');
+            const nCiviqueElement = document.getElementById('nCivique');
+            const nRueElement = document.getElementById('nRue');
+            const cPostalElement = document.getElementById('cPostal');
+            etapeValide =
+                validerChamps(prenomElement)
+                    && validerChamps(nomElement)
+                    && validerChamps(telephoneElement)
+                    && validerEmail(emailElement)
+                    && validerChamps(villeElement)
+                    && validerChamps(nCiviqueElement)
+                    && validerChamps(nRueElement)
+                    && validerChamps(cPostalElement);
+            // if(etapeValide){
+            //     etapeValide = validerChamps(prenomElement);
+            // } 
+            // if(etapeValide){
+            //     etapeValide = validerChamps(nomElement);
+            // } 
+            // if(etapeValide){ 
+            //     etapeValide = validerChamps(telephoneElement); 
+            // } if(etapeValide){ 
+            //     etapeValide = validerChamps(emailElement); 
+            // }
             break;
     }
     return etapeValide;
@@ -103,6 +185,7 @@ function initialiser() {
     btnSuivant = document.getElementById('bouton-suivant');
     btnPrecedent = document.getElementById('bouton-precedent');
     btnSoumettre = document.getElementById('bouton-soumettre');
+    // btnSuivant.addEventListener('input', validerEtape(etape));
     if (btnPrecedent) {
         btnPrecedent.addEventListener('click', naviguerPrecedent);
     }
--- src/ts/scripts.ts
+++ src/ts/scripts.ts
@@ -4,11 +4,16 @@ let btnSuivant: HTMLButtonElement | null;
 let btnPrecedent: HTMLButtonElement | null;
 let btnSoumettre: HTMLButtonElement | null;
 let section: NodeListOf<HTMLElement> | null;
-interface ErreurJson{
-    vide:string;
-    pattern:string;
+interface messageErreur {
+    vide?: string;
+    pattern?: string;
+    type?: string;
+    tldSuspicieux?: string;
 }
-let messagesJSON = {};
+interface erreursJSON {
+    [fieldName: string]: messageErreur;
+}
+let messagesJSON:erreursJSON;
 
 
 function afficherEtape(etape:number):void{
@@ -46,8 +51,11 @@ function cacherSection(){
 }
 
 function naviguerSuivant(event: MouseEvent):void{
+    event.preventDefault(); 
+    if (validerEtape(etape)) {
         etape++;
         afficherEtape(etape);
+    }
     
 }
 function naviguerPrecedent(event: MouseEvent):void{
@@ -67,20 +75,87 @@ async function obtenirMessage(){
 
 function validerChamps(champ: HTMLInputElement): boolean{
     let valide = false;
+    const id = champ.id; // email
+    const idMessageErreur = "erreur-" + id; // erreur-email
+    const labelErreur = "label-" + id;
+    const erreurElement = document.getElementById(idMessageErreur) as HTMLDivElement;
+    const erreurLabel = document.getElementById(labelErreur) as HTMLLabelElement;
+
 // Vérifie chaque type d'erreur de validation
-if (champ.validity.valueMissing && messagesJSON?.vide) {
-    // Champ obligatoire vide (attribut required)
-    valide = false;
-}else if (champ.validity.typeMismatch && messagesJSON?.typeMismatch) {
-    // Type de données incorrect (email, url, tel, etc.)
-    valide = false;
-} else if (champ.validity.patternMismatch && messagesJSON?.patternMismatch) {
-    // Ne correspond pas au pattern regex défini
-    valide = false;
-} else if (champ.validity.valid === false) {
-    // Catch-all pour toute autre erreur de validation non spécifique
-    valide = false;
+    if (champ.validity.valueMissing && messagesJSON[id]?.vide) {
+        console.log('erreur', id);
+        
+        valide = false;
+        erreurElement.innerText = messagesJSON[id]?.vide;
+        erreurLabel.classList.add("erreur");
+
+    } else if (champ.validity.typeMismatch && messagesJSON[id].type) {
+        // Type de données incorrect (email, url, tel, etc.)
+        valide = false;
+        erreurElement.innerText = messagesJSON[id]?.type;
+        erreurLabel.classList.add("erreur");
+    } else if (champ.validity.patternMismatch && messagesJSON[id]?.pattern) {
+        // Ne correspond pas au pattern regex défini
+        valide = false;
+        erreurElement.innerText = messagesJSON[id]?.pattern;
+        erreurLabel.classList.add("erreur");
+    }else{
+        // La validation n'a pas d'erreur, donc on assigne la variable vraie
+        valide = true;
+        erreurElement.innerText = "";
+    }
+    return valide;
 }
+
+function validerEmail(champ:HTMLInputElement): boolean{
+    let valide = false;
+    const id = "email";
+    const leEmail = champ.value;
+    const idMessageErreur = "erreur-" + id; // erreur-email
+    const erreurElement = document.getElementById(idMessageErreur) as HTMLDivElement;
+
+    const tldSuspicieux = [
+        '.ru',
+        '.cn',
+        'click',
+        '.party',
+    ]
+    const erreursCommune = {
+        'hotnail' : 'hotmail',
+
+    }
+
+    const tldValide = tldSuspicieux.some(
+        (tld) =>{
+            const contientSuspect = leEmail.toLowerCase().endsWith(tld);
+            return contientSuspect;
+        });
+    // Vérifie chaque type d'erreur de validation
+    if (champ.validity.valueMissing && messagesJSON[id]?.vide) {
+        // Champ obligatoire vide (attribut required)
+        valide = false;
+        erreurElement.innerText = messagesJSON[id]?.vide;
+    }else if (champ.validity.typeMismatch && messagesJSON[id]?.type) {
+        // Type de données incorrect (email, url, tel, etc.)
+        valide = false;
+        erreurElement.innerText = messagesJSON[id]?.type;
+    } else if (champ.validity.patternMismatch && messagesJSON[id]?.pattern) {
+        // Ne correspond pas au pattern regex défini
+        valide = false;
+        erreurElement.innerText = messagesJSON[id]?.pattern;
+    }else if(tldValide && messagesJSON[id].tldSuspicieux){
+
+        valide = false;
+        erreurElement.innerText = messagesJSON[id].tldSuspicieux;
+
+    }else{
+        const erreursCles = Object.keys(erreursCommune);
+        const erreurCle = erreursCles.find((domaine:string) => {
+            return leEmail.toLowerCase().includes(domaine);
+        })
+        valide = true;
+    }
+    return valide;
 
 }
 
@@ -88,23 +163,44 @@ function validerEtape(etape:number): boolean{
     let etapeValide = false;
     switch(etape){
         case 0:
+            const montantElement = document.getElementById('montant') as HTMLInputElement;
+            etapeValide =
+            validerChamps(montantElement)
+        break;
+
+        case 1:
             const prenomElement = document.getElementById('prenom') as HTMLInputElement;
             const nomElement = document.getElementById('nom')as HTMLInputElement;
             const telephoneElement = document.getElementById('telephone')as HTMLInputElement;
             const emailElement = document.getElementById('email')as HTMLInputElement;
-        
-            etapeValide
-            etapeValide = validerChamps(prenomElement) && validerChamps(nomElement) && validerChamps(nomElement);
+            const villeElement = document.getElementById('ville')as HTMLInputElement;
+            const nCiviqueElement = document.getElementById('nCivique')as HTMLInputElement;
+            const nRueElement = document.getElementById('nRue')as HTMLInputElement;
+            const cPostalElement = document.getElementById('cPostal')as HTMLInputElement;
 
-            if(etapeValide){
-                etapeValide = validerChamps(nomElement);
-            }
-            if(etapeValide){
-                etapeValide = validerChamps(telephoneElement);
-            }
-            if(etapeValide){
-                etapeValide = validerChamps(emailElement);
-            }
+        
+            etapeValide = 
+            validerChamps(prenomElement)
+            && validerChamps(nomElement)
+            && validerChamps(telephoneElement)
+            && validerEmail(emailElement)
+            && validerChamps(villeElement)
+            && validerChamps(nCiviqueElement)
+            && validerChamps(nRueElement)
+            && validerChamps(cPostalElement)
+            ;
+            // if(etapeValide){
+            //     etapeValide = validerChamps(prenomElement);
+            // } 
+            // if(etapeValide){
+            //     etapeValide = validerChamps(nomElement);
+            // } 
+            // if(etapeValide){ 
+            //     etapeValide = validerChamps(telephoneElement); 
+            // } if(etapeValide){ 
+            //     etapeValide = validerChamps(emailElement); 
+            // }
+
         break;
     }
     return etapeValide;
@@ -123,9 +219,11 @@ function initialiser():void{
     btnPrecedent = document.getElementById('bouton-precedent') as HTMLButtonElement;
     btnSoumettre = document.getElementById('bouton-soumettre') as HTMLButtonElement;
 
+    // btnSuivant.addEventListener('input', validerEtape(etape));
     if(btnPrecedent){
         btnPrecedent.addEventListener('click', naviguerPrecedent);
     };
+    
     if(btnSuivant){
         btnSuivant.addEventListener('click', naviguerSuivant);
     };
